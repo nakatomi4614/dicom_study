603f043f2ff820115fc7744ba28220e620dd9f6d
nakatomi4614 <nakatomi4614@gmail.com>
2021-09-24 10:36:44 +0900

    t値の修正以外は完了

--- 単回帰分析と傾き切片の比較.py
+++ 単回帰分析と傾き切片の比較.py
@@ -17,29 +17,45 @@ data = pd.read_csv(data_path_1_1)
 data["mAs"] = data["Exposure Time"]*data["uA"]*10**(-6)
 data.loc[(data['weight'] <= 70) & (data['weight'] >= 50), 'Category'] = 1
 data = data.fillna(0) # 欠損値がある場合は 0 で埋める
-
 # 説明変数と目的変数の指定
 Ev1 = "mAs"
 Ev2 = "weight"
+Ev3 = "Category"
 Ov1 = "CTDIvol"
 Ov2 = "DLP"
 
-# CTDIvol ~ mAs , DLP ~mAs
-analisys_mAs = [Ov1 + "~" + Ev1, Ov2 + "~" + Ev1]
-# CTDIvol ~ weight , DLP ~weight
-analisys_wt = [Ov1 + "~" + Ev2, Ov2 + "~" + Ev2]
+analisys_mAs = [Ov1 + "~" + Ev1, Ov1 + "~" + Ev3+"+"+Ev1,Ov1 +"~"+Ev3+"*"+Ev1 ]
+analisys_DLP_wt = [Ov1 + "~" + Ev2, Ov1 + "~" + Ev3+"+"+Ev2,Ov1 +"~"+Ev3+"*"+Ev2 ]
+#CTDIvol_list = ['CTDIvol ~ mAs','CTDIvol ~ Category+mAs','CTDIvol ~ Category*mAs']
+#DLP_list = ['DLP ~ weight','CTDIvol ~ Category+weight','DLP ~ Category*weight']
+
 ols_list =[]
 for i in analisys_mAs:
     ols = smf.ols(i, data).fit()
     print(ols.summary())
     ols_list.append(ols)
-
-anova_int_dif = sm.stats.anova_lm(ols_list[1] , ols_list[2],typ=1)
-anova_all_int = sm.stats.anova_lm(ols_list[0] , ols_list[1],typ=1)
-print("「切片は異なるが、傾きは同じもの」と「切片も傾きも異なるもの」を比較")
-print(anova_int_dif)
-print("「すべてのカテゴリをまとめて回帰したもの」と「切片は異なるが、傾きは同じもの」を比較")
-print(anova_all_int)
+for i in analisys_DLP_wt:
+    ols = smf.ols(i, data.query("weight != 0")).fit()
+    print(ols.summary())
+    ols_list.append(ols)
+anova_int_dif =[]
+anova_all_int =[]
+for i in range(0,4,3):
+    aid = sm.stats.anova_lm(ols_list[i+1] , ols_list[i+2],typ=1)
+    aai = sm.stats.anova_lm(ols_list[i] , ols_list[i+1],typ=1)
+    anova_int_dif.append(aid)
+    anova_all_int.append(aai)
+for i in range(0,2):
+    msg1="""「切片は異なるが、傾きは同じもの」と「切片も傾きも異なるもの」を比較
+    p<0.05で帰無仮説が棄却されてこの二つの傾きが違うと言える。
+    p>0.05で帰無仮説が採用されこの二つの傾きが違うと言えない。"""
+    print(msg1)
+    print(anova_int_dif[i])
+    msg2 = """「すべてのカテゴリをまとめて回帰したもの」と「切片は異なるが、傾きは同じもの」を比較
+    p<0.05で帰無仮説が棄却されてこの二つの切片が違うと言える。
+    p>0.05で帰無仮説が採用されこの二つの切片が違うと言えない。"""
+    print(msg2)
+    print(anova_all_int[i])
 
 """
 a = ols_list[0].params['Intercept']
